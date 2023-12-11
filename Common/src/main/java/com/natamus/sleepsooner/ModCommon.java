5639dd29a119b14f8ef516526ca496395cedb793
Rick South <rick@natamus.com>
2023-12-12 00:07:26 +0100

    Sleep Sooner source code for 1.20.1.

---
+++ Common/src/main/java/com/natamus/sleepsooner/ModCommon.java
@@ -0,0 +1,17 @@
+package com.natamus.sleepsooner;
+
+import com.natamus.collective.config.GenerateJSONFiles;
+import com.natamus.sleepsooner.config.ConfigHandler;
+import com.natamus.sleepsooner.util.Reference;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		GenerateJSONFiles.requestJSONFile(Reference.MOD_ID, "linger_messages.json");
+	}
+}
