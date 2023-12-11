87a8132027a7dcd9c15994d0f0b087125e839e9e
Rick South <rick@natamus.com>
2023-12-12 00:07:15 +0100

    Sleep Sooner source code for 1.19.4.

---
+++ Common/src/main/java/com/natamus/sleepsooner/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.sleepsooner;
+
+import com.natamus.sleepsooner.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
