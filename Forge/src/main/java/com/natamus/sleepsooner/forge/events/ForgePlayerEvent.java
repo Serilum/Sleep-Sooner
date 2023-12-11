ed8b60084783e3715b0a29328a575ab99f6a938f
Rick South <rick@natamus.com>
2023-12-12 00:07:00 +0100

    Sleep Sooner source code for 1.18.2.

---
+++ Forge/src/main/java/com/natamus/sleepsooner/forge/events/ForgePlayerEvent.java
@@ -0,0 +1,20 @@
+package com.natamus.sleepsooner.forge.events;
+
+import com.natamus.sleepsooner.events.PlayerEvent;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgePlayerEvent {
+	@SubscribeEvent
+	public void playerClick(PlayerInteractEvent.RightClickBlock e) {  
+		Player player = e.getPlayer();
+		Level level = player.getCommandSenderWorld();
+		if (!PlayerEvent.playerClick(level, player, e.getHand(), e.getPos(), e.getHitVec())) {
+			e.setCanceled(true);
+		}
+	}
+}
