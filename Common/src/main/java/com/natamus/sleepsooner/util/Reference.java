95efb3946f0135cca2f67dab8ad0a883cf9d1047
Rick South <rick@natamus.com>
2024-03-09 13:51:32 +0100

    (v4.5) Updated the mod to use the Collective's improved MessageFunctions instead of StringFunctions.

--- Common/src/main/java/com/natamus/sleepsooner/util/Reference.java
+++ Common/src/main/java/com/natamus/sleepsooner/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.sleepsooner.util;
 public class Reference {
 	public static final String MOD_ID = "sleepsooner";
 	public static final String NAME = "Sleep Sooner";
-	public static final String VERSION = "4.4";
+	public static final String VERSION = "4.5";
 	public static final String ACCEPTED_VERSIONS = "[1.20.4]";
 }
