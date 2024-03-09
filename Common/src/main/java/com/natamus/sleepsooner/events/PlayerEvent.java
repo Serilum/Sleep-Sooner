2226383f8c5e036600fe4fededf99e4305dbe19f
Rick South <rick@natamus.com>
2024-03-09 13:51:28 +0100

    (v4.5) Updated the mod to use the Collective's improved MessageFunctions instead of StringFunctions.

--- Common/src/main/java/com/natamus/sleepsooner/events/PlayerEvent.java
+++ Common/src/main/java/com/natamus/sleepsooner/events/PlayerEvent.java
@@ -1,7 +1,7 @@
 package com.natamus.sleepsooner.events;
 
 import com.natamus.collective.data.GlobalVariables;
-import com.natamus.collective.functions.StringFunctions;
+import com.natamus.collective.functions.MessageFunctions;
 import com.natamus.collective.functions.WorldFunctions;
 import com.natamus.sleepsooner.config.ConfigHandler;
 import net.minecraft.ChatFormatting;
@@ -37,7 +37,7 @@ public class PlayerEvent {
 		
 		if (sleeptime > 12540) {
 			if (daytime > 12540 && daytime < sleeptime) {
-				StringFunctions.sendMessage(player, "It's too early to sleep.", ChatFormatting.DARK_GREEN);
+				MessageFunctions.sendMessage(player, "It's too early to sleep.", ChatFormatting.DARK_GREEN);
 				
 				return false;
 			}
@@ -56,7 +56,7 @@ public class PlayerEvent {
 		if (ConfigHandler.enablePreSleepMessage) {
 			String unique = GlobalVariables.lingerMessages.get(GlobalVariables.random.nextInt(GlobalVariables.lingerMessages.size()));
 			
-			StringFunctions.sendMessage(player, "You " + unique + " until dusk. You may now sleep.", ChatFormatting.DARK_GREEN);
+			MessageFunctions.sendMessage(player, "You " + unique + " until dusk. You may now sleep.", ChatFormatting.DARK_GREEN);
 		}
 		
 		return true;
