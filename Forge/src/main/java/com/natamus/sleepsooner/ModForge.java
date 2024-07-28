ea9455afc3e5091da1a9778d88488201232806f2
Rick South <rick@natamus.com>
2024-07-28 07:14:54 +0200

    (v4.6) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Forge/src/main/java/com/natamus/sleepsooner/ModForge.java
+++ Forge/src/main/java/com/natamus/sleepsooner/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.sleepsooner;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.sleepsooner.forge.config.IntegrateForgeConfig;
 import com.natamus.sleepsooner.forge.events.ForgePlayerEvent;
 import com.natamus.sleepsooner.util.Reference;
@@ -15,6 +16,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
