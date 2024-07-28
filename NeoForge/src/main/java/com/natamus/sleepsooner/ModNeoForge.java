552fbf2963c6e68764564e68add65d6012677625
Rick South <rick@natamus.com>
2024-07-28 07:14:57 +0200

    (v4.6) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/sleepsooner/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/sleepsooner/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.sleepsooner;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.sleepsooner.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.sleepsooner.neoforge.events.NeoForgePlayerEvent;
 import com.natamus.sleepsooner.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
