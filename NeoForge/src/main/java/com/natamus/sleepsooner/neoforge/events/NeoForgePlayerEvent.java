9e8b956b73de5e0cd00a0bb08e4ada5cf8591b71
Rick South <rick@natamus.com>
2024-10-26 15:00:09 +0200

    Sleep Sooner source code for 1.21.3.

---
+++ NeoForge/src/main/java/com/natamus/sleepsooner/neoforge/events/NeoForgePlayerEvent.java
@@ -0,0 +1,20 @@
+package com.natamus.sleepsooner.neoforge.events;
+
+import com.natamus.sleepsooner.events.PlayerEvent;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgePlayerEvent {
+	@SubscribeEvent
+	public static void playerClick(PlayerInteractEvent.RightClickBlock e) {  
+		Player player = e.getEntity();
+		Level level = player.level();
+		if (!PlayerEvent.playerClick(level, player, e.getHand(), e.getPos(), e.getHitVec())) {
+			e.setCanceled(true);
+		}
+	}
+}
